2c69f1195142db437dea9c7324e27800e2916c89
srivishnu1644 <srivishnu6648@gmail.com>
2025-05-27 21:57:23 +0530

    Update Hero, Achievements, and About components styles and content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,15 +7,19 @@ const About = () => {
     <div id="about" className="about">
       <div className="about-content">
         <div className="about-left">
+          <h1>About Me</h1>
           <div className="about-para">
             <p>
               Hello! I'm a passionate web developer with a knack for creating dynamic and responsive web applications. I love turning ideas into reality and am always eager to learn new technologies and improve my skills.
+<<<<<<< HEAD
               I have a strong foundation in HTML, CSS, and JavaScript, and I'm proficient in React.js for building interactive user interfaces. In my spare time, I enjoy exploring the latest trends in web development, contributing to open-source projects, and sharing my knowledge with others. Let's connect and build something amazing together!
+=======
+              I have a strong foundation in HTML, CSS, and JavaScript, and I'm proficient in React.js for building interactive user interfaces.In my spare time, I enjoy exploring the latest trends in web development, contributing to open-source projects, and sharing my knowledge with others. Let's connect and build something amazing together!
+>>>>>>> 570a604 (Update Hero, Achievements, and About components styles and content)
             </p>
           </div>
         </div>
         <div className="about-right">
-          <h1>About Me</h1>
           <div className="about-image">
             <img src={profile_img} alt="" />
           </div>

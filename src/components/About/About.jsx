616874de2d75376bdee066c2670ef780dd019752
srivishnu1644 <srivishnu6648@gmail.com>
2025-05-29 11:07:30 +0530

    Update About.jsx file

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,8 +10,8 @@ const About = () => {
           <h1>About Me</h1>
           <div className="about-para">
             <p>
-              Hello! I'm a passionate web developer with a knack for creating dynamic and responsive web applications. I love turning ideas into reality and am always eager to learn new technologies and improve my skills.I have a strong foundation in HTML, CSS, and JavaScript, and I'm proficient in React.js for building interactive user interfaces.In my spare time, I enjoy exploring the latest trends in web development, contributing to open-source projects, and sharing my knowledge with others. Let's connect and build something amazing together!
-
+              Hello! I'm a passionate web developer with a knack for creating dynamic and responsive web applications. I love turning ideas into reality and am always eager to learn new technologies and improve my skills.
+              I have a strong foundation in HTML, CSS, and JavaScript, and I'm proficient in React.js for building interactive user interfaces. In my spare time, I enjoy exploring the latest trends in web development, contributing to open-source projects, and sharing my knowledge with others. Let's connect and build something amazing together!
             </p>
           </div>
         </div>
